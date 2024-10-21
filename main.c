ad1c3c24af30e53539b10945f9cb288c9cfa7f0f
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-21 20:29:04 +0200

    new

--- main.c
+++ main.c
@@ -1,54 +1,52 @@
-#include <stdio.h>
-#include <math.h>
 #include <raylib.h>
+#include <raymath.h>
 
 #define square(x) (x)*(x)
 
+const int X = 600;
+const int Y = 600;
+const int shiftx = 100;
+
+const double zoomy = 2.f;
+const double zoomx = 3.f;
+
+const int max_iterations = 100;
+
+void mandelbrot();
+
 int main() {
-    const int X = 600;
-    const int Y = 600;
-    const int shiftx = 100;
-
-    const double zoomy = 2.f;
-    const double zoomx = 3.f;
-
-    const int max_iterations = 100;
-
     InitWindow(X, Y, "Mandelbrot");
     SetTargetFPS(30);
 
+    Camera2D camera = {0};
+    camera.zoom = 1.0f;
+
     while (!WindowShouldClose()) {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f/camera.zoom);
+            camera.target = Vector2Add(camera.target, delta);
+        }
+
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0) {
+            camera.offset = GetMousePosition();
+
+            float scale_factor = 1.0f + (0.25f * fabsf(wheel));
+            if (wheel < 0) 
+                scale_factor = 1.0f / scale_factor;
+
+            camera.zoom = Clamp(camera.zoom * scale_factor, 0.125f, 64.0f);
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        for (int y = -Y/2; y < Y/2; y++) {
-            for (int x = -X/2; x < X/2; x++) {
-                double a = (double)x / (double)X * zoomx;
-                double b = (double)y / (double)Y * zoomy;
+            BeginMode2D(camera);
 
-                double pa = a;
-                double pb = b;
+                mandelbrot();
 
-                int n = 0;
-                while (n < max_iterations) {
-                    double re = square(a) - square(b);
-                    double im = 2 * a * b;
-
-                    a = re + pa;
-                    b = im + pb;
-
-                    if (square(a) + square(b) > 2)
-                        break;
-
-                    n++;
-                }
-                
-                double brightness = (double)n / (double)max_iterations * 255.f;
-
-                Color col = {brightness, brightness, brightness, 255};
-                DrawPixel(x+X/2+shiftx, y+Y/2, col);
-            }
-        }
+            EndMode2D();
 
         EndDrawing();
     }
@@ -56,3 +54,35 @@ int main() {
     CloseWindow();
     return 0;
 }
+
+void mandelbrot() {
+    for (int y = -Y/2; y < Y/2; y++) {
+        for (int x = -X/2; x < X/2; x++) {
+            double a = (double)x / (double)X * zoomx;
+            double b = (double)y / (double)Y * zoomy;
+
+            double pa = a;
+            double pb = b;
+
+            int n = 0;
+            while (n < max_iterations) {
+                double re = square(a) - square(b);
+                double im = 2 * a * b;
+
+                a = re + pa;
+                b = im + pb;
+
+                if (square(a) + square(b) > 2)
+                    break;
+
+                n++;
+            }
+            
+            double brightness = (double)n / (double)max_iterations * 255.f;
+
+            Color col = {brightness, brightness, brightness, 255};
+            DrawPixel(x+X/2+shiftx, y+Y/2, col);
+        }
+    }
+}
+
